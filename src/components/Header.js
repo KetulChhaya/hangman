a1e17bd033f178114829448503f5dd666832e61a
Ketul Chhaya <chhayaketul@gmail.com>
2021-06-24 19:00:35 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <>
+          <h1 style={{textAlign: 'center'}}>HangMan</h1>  
+          <p style={{textAlign:'center'}}>Find the hidden word - Enter a letter</p>
+        </>
+    )
+}
+
+export default Header
